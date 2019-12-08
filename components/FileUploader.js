3e8dd0242e4c04c787e9179d9335213cf3680028
hophacker <j@paiyou.co>
2019-12-09 02:37:22 +0800

    implement out own FileUploader.js using raw code

---
+++ components/FileUploader.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { Upload, Button, Icon, Spin } from 'antd'
+
+const uploadProps = {
+  action: 'http://upload.qiniup.com/',
+  name: 'file'
+}
+
+export default ({ token, onSuccess, keyPrefix = 'file' }) => {
+  const [uploading, setUploading] = useState(false)
+  const [name, setName] = useState('')
+
+  const handleAvatarChange = ({ file }) => {
+    console.info(file)
+    if (file.status === 'uploading') {
+      setUploading(true)
+      return
+    }
+    if (file.status === 'done') {
+      setUploading(false)
+      const {
+        response: { key }
+      } = file
+      onSuccess(`http://res.paiyou.co/${key}`)
+    }
+  }
+
+  return (
+    <Upload
+      {...uploadProps}
+      accept="image/*"
+      showUploadList={false}
+      beforeUpload={file => setName(file.name)}
+      data={{ token, key: `${keyPrefix}_${new Date().getTime()}_${name}` }}
+      onChange={handleAvatarChange}
+    >
+      <Button>
+        {uploading ? <Spin indicator={<Icon type="loading" style={{ fontSize: 18 }} spin />} /> : <Icon type="upload" />}
+        <span className="ML-2">Upload</span>
+      </Button>
+    </Upload>
+  )
+}
