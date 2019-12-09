0bf8b47721e8983bf8092466f2851f1721d33ac8
hophacker <j@paiyou.co>
2019-12-09 22:33:36 +0800

    rename

---
+++ utils/authedCctionCable.js
@@ -0,0 +1,17 @@
+import dns from './dns'
+import { getAuthorization } from './request'
+import { viewSetIn, authorizedPath } from '%view'
+import ActionCable from '../public/action_cable'
+
+export const authorization2ActionCable = {}
+export default () => {
+  const authorization = getAuthorization()
+  if (!authorization) {
+    DISPATCH(viewSetIn(authorizedPath, false))
+    return null
+  }
+  if (!authorization2ActionCable[authorization]) {
+    authorization2ActionCable[authorization] = ActionCable.createConsumer(`${dns.API_ROOT}/cable`, (authorization || '').split(' ')[1])
+  }
+  return authorization2ActionCable[authorization]
+}
