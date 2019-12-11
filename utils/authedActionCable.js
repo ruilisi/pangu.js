10b4dac116e3c3433c90e3e06b8952535ef20ae0
hophacker <j@paiyou.co>
2019-12-12 07:55:28 +0800

    use actioncable-jwt

--- utils/authedActionCable.js
+++ utils/authedActionCable.js
@@ -1,4 +1,4 @@
-import ActionCable from 'actioncable-rails'
+import { createConsumer } from 'actioncable-jwt'
 import dns from './dns'
 import { getAuthorization } from './request'
 import { viewSetIn, authorizedPath } from '%view'
@@ -11,7 +11,7 @@ export default () => {
     return null
   }
   if (!authorization2ActionCable[authorization]) {
-    authorization2ActionCable[authorization] = ActionCable.createConsumer(`${dns.API_ROOT}/cable`, (authorization || '').split(' ')[1])
+    authorization2ActionCable[authorization] = createConsumer(`${dns.API_ROOT}/cable`, (authorization || '').split(' ')[1])
   }
   return authorization2ActionCable[authorization]
 }
