44e554b05068a5f7c5f88b4c8852bffca9f6530e
hophacker <j@paiyou.co>
2019-12-12 17:53:24 +0800

    new domain

--- utils/dns.js
+++ utils/dns.js
@@ -1,5 +1,5 @@
 export default {
   API_ROOT: '',
   API_ROOT_LOCAL: 'http://localhost',
-  REMOTE_HOSTS: ['https://limitless-falls-17517.herokuapp.com']
+  REMOTE_HOSTS: ['https://pangu.lingti.io']
 }
