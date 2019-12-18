e1d0daabed0a2704489ccc4b9bf50d9db8b10e06
wsq <wsq@paiyou.co>
2019-12-18 14:19:31 +0800

    input autosize, fix layout

--- pages/chat.js
+++ pages/chat.js
@@ -68,7 +68,7 @@ const Chat = () => {
     })
   }, [])
 
-  const onKeyPress = e => {
+  const onKeyDown = e => {
     if (e.key === 'Enter' && !e.shiftKey) {
       if (text.trim() === '') return
       channel.load('add_message', { room_id: roomId, text })
@@ -95,73 +95,82 @@ const Chat = () => {
           }}
         />
       </Modal>
-      <Col span={4} className="border-card" style={{ background: '#3f0e40', overflow: 'hidden' }}>
-        <div className="FS-10 TA-C PT-20">
-          <Row style={{ display: 'flex', alignItems: 'center' }}>
-            <Col span={12}>
-              <Avatar src={self.getIn(['data', 'avatar'])} shape="circle" size="large" />
-            </Col>
-            <Col span={12}>
-              <Setting switchRoom={switchRoom} />
-            </Col>
-          </Row>
-        </div>
-        <Card style={{ background: '#3f0e40', height: '20vh', overflowY: 'scroll' }} bordered={false}>
-          <Rooms rooms={rooms} switchRoom={switchRoom} />
-        </Card>
-        <Card style={{ background: '#3f0e40', height: '60vh', overflowY: 'scroll' }} bordered={false}>
-          <UserList id={roomId} style={{ height: '70vh' }} />
-        </Card>
-      </Col>
-      {!roomId ? (
-        <Col span={20} style={{ textAlign: 'center', height: '100vh', lineHeight: '100vh' }}>
-          新建或加入一个房间开始你的聊天吧
-        </Col>
-      ) : (
-        <Col span={20} style={{ overflow: 'hidden' }}>
-          <div className="FS-10 ML-5" style={{ height: '10vh' }}>
-            {rooms.toJS()[roomId] === undefined ? '' : rooms.toJS()[roomId].title}
-          </div>
-          <Row>
-            <Card style={{ height: '80vh', overflowY: 'scroll' }} bordered={false}>
-              {room.get('messages', List()).map(v => (
-                <Row key={v.get('id')}>
-                  <Col span={1} className="ML-5">
-                    <Avatar src={avatars[v.get('user_id')]} />
-                  </Col>
-                  <Col span={10}>
-                    <div className="bold FS-7">
-                      {v.getIn(['data', 'email'])}
-                      <span style={{ fontWeight: 'lighter', color: 'grey', fontSize: 12, marginLeft: 10 }}>
-                        {new Date(v.get('created_at')).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}
-                      </span>
-                    </div>
-                    <div dangerouslySetInnerHTML={{ __html: md.render(v.get('text')) }} />
-                  </Col>
-                </Row>
-              ))}
-            </Card>
-            <div className="TA-C" style={{ height: '10vh', display: 'flex', alignItems: 'center' }}>
-              <Col className="display-center" span={18} push={2}>
-                <Input.TextArea
-                  value={text}
-                  size="large"
-                  placeholder="随便吐槽一下吧"
-                  style={{ background: '#e1e1e1', width: '100%' }}
-                  onChange={e => {
-                    setText(e.target.value)
-                  }}
-                  onKeyPress={onKeyPress}
-                />
+      <Row>
+        <Col span={4} className="border-card" style={{ height: '100vh', background: '#3f0e40', overflow: 'hidden' }}>
+          <div className="FS-10 TA-C PT-20">
+            <Row style={{ display: 'flex', alignItems: 'center' }}>
+              <Col span={12}>
+                <Avatar src={self.getIn(['data', 'avatar'])} shape="circle" size="large" />
               </Col>
-            </div>
-          </Row>
+              <Col span={12}>
+                <Setting />
+              </Col>
+            </Row>
+          </div>
+          <Card style={{ background: '#3f0e40', height: '20vh', overflowY: 'scroll' }} bordered={false}>
+            <Rooms rooms={rooms} switchRoom={switchRoom} />
+          </Card>
+          <Card style={{ background: '#3f0e40', height: '60vh', overflowY: 'scroll' }} bordered={false}>
+            <UserList id={roomId} style={{ height: '70vh' }} />
+          </Card>
         </Col>
-      )}
+        {!roomId ? (
+          <Col span={20} style={{ textAlign: 'center', height: '100vh', lineHeight: '100vh' }}>
+            新建或加入一个房间开始你的聊天吧
+          </Col>
+        ) : (
+          <Col span={20} style={{ height: '100vh', overflow: 'hidden' }}>
+            <div className="FS-10 ML-5" style={{ height: '10vh' }}>
+              {rooms.toJS()[roomId] === undefined ? '' : rooms.toJS()[roomId].title}
+            </div>
+            <Row>
+              <Card style={{ height: '80vh', overflowY: 'scroll' }} bordered={false}>
+                {room.get('messages', List()).map(v => (
+                  <Row key={v.get('id')}>
+                    <Col span={1} className="ML-5">
+                      <Avatar src={avatars[v.get('user_id')]} />
+                    </Col>
+                    <Col span={10}>
+                      <div className="bold FS-7">
+                        {v.getIn(['data', 'email'])}
+                        <span style={{ fontWeight: 'lighter', color: 'grey', fontSize: 12, marginLeft: 10 }}>
+                          {new Date(v.get('created_at')).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}
+                        </span>
+                      </div>
+                      <div dangerouslySetInnerHTML={{ __html: md.render(v.get('text')) }} />
+                    </Col>
+                  </Row>
+                ))}
+              </Card>
+            </Row>
+            <div className="TA-C bottom-input">
+              <Input.TextArea
+                value={text}
+                autoSize={{ minRows: 1, maxRows: 10 }}
+                placeholder="随便吐槽一下吧"
+                style={{ background: '#ffffff', width: '100%', margin: '30px 20px' }}
+                onChange={e => {
+                  setText(e.target.value)
+                }}
+                onKeyDown={onKeyDown}
+              />
+            </div>
+          </Col>
+        )}
+      </Row>
       <style jsx global>
         {`
           .ant-menu-vertical {
             border: none;
+          }
+          .bottom-input {
+            height: auto;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            background: #e9f5fa;
+            position: absolute;
+            bottom: 0px;
           }
           .border-card {
             height: 100vh;
