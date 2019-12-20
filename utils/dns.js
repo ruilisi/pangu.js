7f12a9acfb027441ba48c1e5b8094be1f4002dab
hophacker <j@paiyou.co>
2019-12-20 13:54:31 +0800

    change remote api

--- utils/dns.js
+++ utils/dns.js
@@ -1,5 +1,5 @@
 export default {
-  API_ROOT: 'https://pangu.lingti.io/',
-  API_ROOT_LOCAL: 'http://localhost',
-  REMOTE_HOSTS: ['https://pangu.lingti.io']
+  API_ROOT: 'https://api.pangu.lingti.io',
+  API_ROOT_LOCAL: 'http://192.168.1.35:8080',
+  REMOTE_HOSTS: ['https://api.pangu.lingti.io']
 }
