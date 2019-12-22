3e0d5c8090f7625a96d85a3f47628e7eb1d70b17
hophacker <j@paiyou.co>
2019-12-22 15:19:16 +0800

    consolidate configureStores

--- redux/configureStore.prod.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from 'redux'
-import thunk from 'redux-thunk'
-import makeRootReducer from './reducers'
-
-export default (initialState = {}) => {
-  const store = createStore(makeRootReducer(), initialState, applyMiddleware(thunk))
-  return store
-}
