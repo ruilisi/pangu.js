6cbf040f81931cedf461ecd3451d2da1fcc7fda0
wsq <wsq@paiyou.co>
2019-12-25 18:02:28 +0800

    ctrl v send image

--- components/MessageInput.js
+++ components/MessageInput.js
@@ -1,15 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import fetch from 'isomorphic-unfetch'
 import { useDispatch, useSelector } from 'react-redux'
 import { Picker } from 'emoji-mart'
 import { Button, Input, Popover } from 'antd'
 import { viewMergeIn, viewSetIn } from '%view'
+import { get } from '../utils/request'
 
 export default ({ subscription, roomId, defaultText = '' }) => {
   const dp = useDispatch()
   const [text, setText] = useState(defaultText)
   const [cursorStart, setCursorStart] = useState(0)
+  const [token, setToken] = useState(0)
   const view = useSelector(state => state.view)
   const messageId = view.getIn(['messageId'])
+  useEffect(() => {
+    get('qiniu_token').then(data => {
+      setToken(data.qiniuToken)
+    })
+  }, [])
 
   const onKeyDown = e => {
     if (e.key === 'Enter' && !e.shiftKey) {
@@ -34,6 +42,27 @@ export default ({ subscription, roomId, defaultText = '' }) => {
       }
       e.preventDefault()
     }
+  }
+
+  const upload = file => {
+    const formData = new FormData()
+    formData.append('key', `screenshot-${new Date().getTime()}-${file.name}`)
+    formData.append('token', token)
+    formData.append('file', file)
+
+    fetch('https://upload.qiniup.com/', {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => response.json())
+      .then(body => {
+        if (body.key) {
+          subscription.perform('load', { path: 'add_message', data: { room_id: roomId, text: `![pic](http://res.paiyou.co/${body.key})` } })
+        } else {
+          console.info(body.error)
+        }
+      })
+      .catch(error => console.info(error))
   }
 
   return (
@@ -67,6 +96,15 @@ export default ({ subscription, roomId, defaultText = '' }) => {
         onKeyDown={onKeyDown}
         onKeyUp={e => setCursorStart(e.target.selectionStart)}
         onClick={e => setCursorStart(e.target.selectionStart)}
+        onPaste={e => {
+          const { items } = e.clipboardData
+          items.forEach(item => {
+            if (item.kind === 'file' && item.type === 'image/png') {
+              upload(item.getAsFile())
+              e.preventDefault()
+            }
+          })
+        }}
       />
     </div>
   )
