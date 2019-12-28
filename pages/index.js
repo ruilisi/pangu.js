7687af0a00fe4563e452de7892aa5c992c9d1558
hophacker <j@paiyou.co>
2019-12-28 14:48:23 +0800

    example of dealing with passing styled jsx down to nested components

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,34 @@
 import React from 'react'
 import { Button, Carousel, Row, Col } from 'antd'
 import Router from 'next/router'
+import css from 'styled-jsx/css'
 import Nav from '../components/nav'
+import ResponsiveImg from '../components/ResponsiveImg'
+
+const { className, styles } = css.resolve`
+  img {
+    height: 355px;
+    width: 623px;
+  }
+  .ant-btn {
+    width: 220px;
+    height: 64px;
+    font-size: 24px;
+  }
+`
 
 const w = 'static/imgs/world.png'
 const d = 'static/imgs/deploy.png'
 const s = 'static/imgs/switch.png'
 
-const LandingPage = () => {
+const IntroSec = () => {
   const grid = {
     xs: {
       span: 24
     },
     md: {
-      offset: 3,
-      span: 18
+      offset: 4,
+      span: 16
     }
   }
   return (
@@ -27,30 +41,29 @@ const LandingPage = () => {
               <div className="name">Pangu.js</div>
               <div className="slogan">React fullstack starter kit focusing on realtime app</div>
               <div className="actions">
-                <Button type="primary" onClick={() => Router.push('/signup')}>
+                <Button type="primary" onClick={() => Router.push('/signup')} className={className}>
                   Get Started
                 </Button>
-                <Button type="secondary" className="ML-20" onClick={() => window.open('https://github.com/ruilisi/pangu.js')}>
+                <Button type="secondary" onClick={() => window.open('https://github.com/ruilisi/pangu.js')} className={`${className} ML-20`}>
                   GitHub
                 </Button>
               </div>
             </Col>
-            <Col span={12}>image</Col>
+            <Col span={12}>
+              <ResponsiveImg name="newton-cradle" className={className} />
+            </Col>
           </Col>
         </Row>
       </div>
       <style jsx>{`
         .index-bg {
           margin-top: 90px;
-          text-align: center;
           background-size: cover;
           background: rgba(221, 222, 230, 1);
           background-position: center;
-          height: 600px;
         }
         .index-bg :global(.ant-btn) {
           width: 220px;
-          height: 64px;
           font-size: 24px;
         }
         .name {
@@ -66,41 +79,49 @@ const LandingPage = () => {
           color: rgba(51, 51, 51, 1);
         }
         .actions {
-          text-align: center;
-          position: absolute;
+          margin-top: 79px;
+          margin-bottom: 140px;
+          width: 100%;
           top: 346px;
         }
       `}</style>
+      {styles}
     </div>
+  )
+}
+
+const FeaturesSec = () => {
+  return (
+    <Row className="index-content container">
+      <div className="TA-C MT-60 FS-18 bold">FEATURES</div>
+      {[
+        [w, 'switching between local and remote', 'Switching'],
+        [s, 'One click deployment of the client', 'Deployment'],
+        [d, 'react-intl allows to switch language', 'Translation']
+      ].map(v => (
+        <Col sm={8} xs={24} style={{ padding: 10 }} key={v[1]}>
+          <div>
+            <div style={{ borderRadius: 8, boxShadow: '0px 3px 14px 1px rgba(0, 0, 0, 0.1)' }} className="TA-C">
+              <div style={{ padding: 40, background: '#5F6DB0', borderTopLeftRadius: 8, borderTopRightRadius: 8 }}>
+                <img src={v[0]} style={{ width: '80%' }} alt="" />
+              </div>
+              <div style={{ padding: 10 }}>
+                <p className="FS-10 bold">{v[2]}</p>
+                <p>{v[1]}</p>
+              </div>
+            </div>
+          </div>
+        </Col>
+      ))}
+    </Row>
   )
 }
 
 const IndexPage = () => {
   return (
     <Nav>
-      <LandingPage />
-      <Row className="index-content container">
-        <div className="TA-C MTB-20 FS-12 bold">Unique core advantages</div>
-        {[
-          [w, 'switching between local and remote', 'Switching'],
-          [s, 'One click deployment of the client', 'Deployment'],
-          [d, 'react-intl allows to switch language', 'Translation']
-        ].map(v => (
-          <Col sm={8} xs={24} style={{ padding: 10 }} key={v[1]}>
-            <div>
-              <div style={{ borderRadius: 8, boxShadow: '0px 3px 14px 1px rgba(0, 0, 0, 0.1)' }} className="TA-C">
-                <div style={{ padding: 40, background: '#5F6DB0', borderTopLeftRadius: 8, borderTopRightRadius: 8 }}>
-                  <img src={v[0]} style={{ width: '80%' }} alt="" />
-                </div>
-                <div style={{ padding: 10 }}>
-                  <p className="FS-10 bold">{v[2]}</p>
-                  <p>{v[1]}</p>
-                </div>
-              </div>
-            </div>
-          </Col>
-        ))}
-      </Row>
+      <IntroSec />
+      <FeaturesSec />
       <Row className="index-content container TA-C">
         <div className="TA-C MTB-20 FS-12 bold">They comment like this</div>
         <Carousel
@@ -135,19 +156,6 @@ const IndexPage = () => {
           </Col>
         ))}
       </Row>
-      <style jsx>
-        {`
-          .border-bg {
-            background: url(/static/imgs/border.png) no-repeat;
-            display: flex;
-            align-items: center;
-            width: 100%;
-            height: auto;
-            justify-content: center;
-            margin-bottom: 20px;
-          }
-        `}
-      </style>
     </Nav>
   )
 }
