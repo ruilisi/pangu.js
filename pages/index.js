62db6bd988e0d5274084adc17400e394110aad78
hophacker <j@paiyou.co>
2019-12-28 14:53:56 +0800

    make doc clear

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,6 @@ const { className, styles } = css.resolve`
   }
 `
 
-const w = 'static/imgs/world.png'
-const d = 'static/imgs/deploy.png'
-const s = 'static/imgs/switch.png'
-
 const IntroSec = () => {
   const grid = {
     xs: {
@@ -41,8 +37,8 @@ const IntroSec = () => {
               <div className="name">Pangu.js</div>
               <div className="slogan">React fullstack starter kit focusing on realtime app</div>
               <div className="actions">
-                <Button type="primary" onClick={() => Router.push('/signup')} className={className}>
-                  Get Started
+                <Button type="primary" onClick={() => window.open('https://pangu.ruilisi.co')} className={className}>
+                  DEMO
                 </Button>
                 <Button type="secondary" onClick={() => window.open('https://github.com/ruilisi/pangu.js')} className={`${className} ML-20`}>
                   GitHub
@@ -90,57 +86,10 @@ const IntroSec = () => {
   )
 }
 
-const FeaturesSec = () => {
-  return (
-    <Row className="index-content container">
-      <div className="TA-C MT-60 FS-18 bold">FEATURES</div>
-      {[
-        [w, 'switching between local and remote', 'Switching'],
-        [s, 'One click deployment of the client', 'Deployment'],
-        [d, 'react-intl allows to switch language', 'Translation']
-      ].map(v => (
-        <Col sm={8} xs={24} style={{ padding: 10 }} key={v[1]}>
-          <div>
-            <div style={{ borderRadius: 8, boxShadow: '0px 3px 14px 1px rgba(0, 0, 0, 0.1)' }} className="TA-C">
-              <div style={{ padding: 40, background: '#5F6DB0', borderTopLeftRadius: 8, borderTopRightRadius: 8 }}>
-                <img src={v[0]} style={{ width: '80%' }} alt="" />
-              </div>
-              <div style={{ padding: 10 }}>
-                <p className="FS-10 bold">{v[2]}</p>
-                <p>{v[1]}</p>
-              </div>
-            </div>
-          </div>
-        </Col>
-      ))}
-    </Row>
-  )
-}
-
 const IndexPage = () => {
   return (
     <Nav>
       <IntroSec />
-      <FeaturesSec />
-      <Row className="index-content container TA-C">
-        <div className="TA-C MTB-20 FS-12 bold">They comment like this</div>
-        <Carousel
-          autoplay
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: 200,
-            fontSize: 30
-          }}
-          dots={false}
-        >
-          <div>This is really convenient for me, I love it so much!</div>
-          <div>I have never used so good tool!</div>
-          <div>If you want to be an excellent developer, please fork it!</div>
-          <div>This is amazing project, damn it!</div>
-        </Carousel>
-      </Row>
       <Row className="index-content container TA-C">
         <div className="TA-C MTB-20 FS-12 bold">Projects Using Pangu.js</div>
         {[
